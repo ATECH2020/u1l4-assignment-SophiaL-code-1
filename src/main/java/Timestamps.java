ce42592a8dbed3c8d8017025acc9d1cc0504a621
Sophia Lindsey <sophia.2312279@nv.ccsd.net>
2020-09-06 00:20:12 +0000

    Added math for TimeStamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -21,14 +21,29 @@ class Timestamps {
         int bHr = hours1; //renaming to help me understand
         int aHr = hours2; //a stands for after b stands for before
         int diffHrs; //difference of hours
+        
         int bMin = minutes1;
         int aMin = minutes2;
         int diffMin; //difference of minutes
+        
         int bSec = seconds1;
-        int aSec = seconds2:
+        int aSec = seconds2;
         int diffSec; //difference of seconds
 
-        
+        int hrSec;
+        int minSec;
+        int sec;
+
+        diffHrs = bHr - aHr;
+        diffMin = bMin - aMin;
+        diffSec = bSec - aSec;
+
+        hrSec = diffHrs * 60 * 60;
+        minSec = diffMin * 60;
+        sec = diffSec;
+
+        System.out.print(hrSec + minSec + sec);
+
 
         // closing the scanner object
         scanner.close();
