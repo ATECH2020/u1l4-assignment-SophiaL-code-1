831c517b92eac4124b6b08514f1989a827591c3b
Sophia Lindsey <sophia.2312279@nv.ccsd.net>
2020-09-06 02:42:22 +0000

    Finished AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,15 +7,17 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        //int hrHand = scanner.nextInt();
-        int a;
-        a = 22;
+        int hrHand = scanner.nextInt();
+
         /*
          *  your code goes here
          */
-          int test = a / 3;
-          System.out.println(test);
-
+          int hours = hrHand / 30;
+          int degPast = hours * 30;
+          int minPast = hrHand - degPast;
+          int minHandDeg = (30 - minPast) * 6;
+          System.out.print(minHandDeg);
+      
 
         // closing the scanner object
         scanner.close();
