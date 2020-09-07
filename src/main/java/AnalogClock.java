6941da86488297f635541e1a46a7d9f6c6f41fbe
Sophia Lindsey <sophia.2312279@nv.ccsd.net>
2020-09-07 05:24:28 +0000

    Adjusted AnalogClock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -15,9 +15,9 @@ class AnalogClock {
           int hours = hrHand / 30; //hours past without remainder
           int degPast = hours * 30; //deg of integer hours
           int minPast = hrHand - degPast; //original - integer hr deg
-          int finalDeg = (30 - minPast) * 6;
+          int finalDeg = minPast * 2;
+          finalDeg = finalDeg * 6;
           System.out.print(finalDeg);
-      
 
         // closing the scanner object
         scanner.close();
